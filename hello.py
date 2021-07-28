4feba3569c7b30dacf285ad365d85498e5b7b1a1
willcline613 <willcline613@gmail.com>
2021-07-27 22:48:01 -0500

    hello update

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/portfolio")
 def portfolio():
